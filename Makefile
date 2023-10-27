61c6343f43f29e7da6bf1b8ac7bed6499bf3dce0
Younes Manton <ymanton@ca.ibm.com>
2023-10-26 23:55:21 -0500

    WIP: pidplus

--- Makefile
+++ Makefile
@@ -1,14 +1,31 @@
 all: pidplus
 
+pidplus: CFLAGS=-g -O0 --coverage -Wpedantic
+
 .PHONY: test
 
 test: pidplus
+	$(RM) pidplus.gcda
 	sudo setcap cap_checkpoint_restore=eip ./pidplus
 	./pidplus
 	./pidplus --
+	./pidplus -h
 	./pidplus echo
 	./pidplus -- echo
 	./pidplus sh -c 'echo "My pid is $$$$ and my parent is $$PPID"'
 	./pidplus -- sh -c 'echo "My pid is $$$$ and my parent is $$PPID"'
 	./pidplus -p 10000 sh -c 'echo "My pid is $$$$ and my parent is $$PPID"'
-	./pidplus -p 10000 -- sh -c 'echo "My pid is $$$$ and my parent is $$PPID"'
+	./pidplus -p 10001 -- sh -c 'echo "My pid is $$$$ and my parent is $$PPID"'
+	./pidplus -p 0 echo && false || true
+	./pidplus -p -1 echo && false || true
+	./pidplus -p 1 echo && false || true
+	./pidplus -p -9223372036854775808 echo && false || true
+	./pidplus -p -9223372036854775809 echo && false || true
+	./pidplus -p 9223372036854775807 echo && false || true
+	./pidplus -p 9223372036854775808 echo && false || true
+	./pidplus -p badpid echo && false || true
+	./pidplus -p 1badpid echo && false || true
+	./pidplus -x echo && false || true
+	./pidplus -x 10000 echo && false || true
+	./pidplus badprog && false || true
+	gcov pidplus.c
--- pidplus.c
+++ pidplus.c
@@ -7,46 +7,83 @@
 #include <stdlib.h>         /* Definition of EXIT_FAILURE */
 #include <errno.h>
 #include <stdio.h>          /* Definition of printf */
+#include <limits.h>         /* Definition of INT_MIN, INT_MAX */
+#include <stdbool.h>        /* Definition of bool */
+
+void help(FILE *stream, const char *progname)
+{
+    fprintf(stream, "%s [OPTIONS...] [--] [COMMAND [ARGS...]]\n"
+                    "Options:\n"
+                    "\t-p <PID> Target PID\n"
+                    "\t-h Help (this text)\n", progname);
+}
 
 extern char *optarg;
 extern int optind, opterr, optopt;
 
-pid_t parse_options(int argc, char **argv, int &target_cmdline_start) {
+pid_t parse_options(int argc, char **argv, int *target_cmdline_start) {
+    const char *progname = argv[0];
     pid_t target_pid = -1;
     int opt = -1;
+
     /* Note that our getopt() optstring starts with + because we need POSIXLY_CORRECT behaviour. The target may have its own
        command-line options and we don't want to process those nor do we want getopt() to transmute argv. If we don't enable
        POSIXLY_CORRECT mode we would need "--" between this program and its options and the target and it's command-line. */
     while ((opt = getopt(argc, argv, "+p:h")) != -1) {
         switch (opt) {
             case 'p':
-                target_pid = atoi(optarg);
+            {
+                bool pid_parsed = false;
+                char *optarg_end = NULL;
+                long target_pid_long = strtol(optarg, &optarg_end, 0);
+
+                if (*optarg_end != '\0')
+                    fprintf(stderr, "Invalid argument to -p: %s\n", optarg);
+                else if (errno == ERANGE)
+                    fprintf(stderr, "Argument to -p out of range for long: %s\n", optarg);
+                else if (target_pid_long < INT_MIN || target_pid_long > INT_MAX)
+                    fprintf(stderr, "Argument to -p out of range for pid_t: %s\n", optarg);
+                else
+                    pid_parsed = true;
+
+                if (!pid_parsed) {
+                    help(stderr, progname);
+                    exit(EXIT_FAILURE);
+                }
+
+                target_pid = target_pid_long;
+
                 break;
+            }
             case 'h':
-                printf("TODO: help text\n");
+                help(stdout, progname);
                 break;
             case '?':
                 fprintf(stderr, "Invalid option or option argument\n");
-                printf("TODO: help text\n");
+                help(stderr, progname);
                 exit(EXIT_FAILURE);
                 break;
             default:
                 fprintf(stderr, "getopt() failed and returned %d\n", opt);
-                printf("TODO: help text\n");
+                help(stderr, progname);
                 exit(EXIT_FAILURE);
                 break;
         }
     }
-    target_cmdline_start = optind;
+
+    if (target_cmdline_start)
+        *target_cmdline_start = optind;
+
     return target_pid;
 }
 
 int main(int argc, char **argv)
 {
-    int target_cmdline_start;
-    pid_t target_pid = parse_options(argc, argv, target_cmdline_start);
+    int target_cmdline_start = -1;
+    pid_t target_pid = parse_options(argc, argv, &target_cmdline_start);
+
     struct clone_args cl_args = {
-        .flags = CLONE_VFORK | /* Wait for the cloned process to exec() before returning; keeps output tidy. */
+        .flags = CLONE_VFORK | /* Wait for the cloned process to exec() before returning; helps to keep our output separate from the target's. */
                  CLONE_PARENT, /* Make the target process a sibling of this process rather than a child. */
         .pidfd = 0,
         .child_tid = 0,
@@ -59,26 +96,27 @@ int main(int argc, char **argv)
         .set_tid_size = target_pid < 0 ? (unsigned long long)0 : (unsigned long long)1,
         .cgroup = 0
     };
+
     long ret = syscall(SYS_clone3, &cl_args, sizeof(cl_args));
+
     if (ret == -1) {
         error_at_line(EXIT_FAILURE, errno, __FILE__, __LINE__, "clone() failed");
     }
     else if (ret == 0) {
-        printf("Success, cloned a process, pid: %d\n", getpid());
+        pid_t current_pid = getpid();
+        if (target_pid >= 0 && current_pid != target_pid)
+            error_at_line(EXIT_FAILURE, 0, __FILE__, __LINE__, "Target process has PID %d, expected PID %d", current_pid, target_pid);
+
         char **target_argv = (char **)malloc(sizeof(char*) * argc - target_cmdline_start + 1);
         for (int i = target_cmdline_start; i < argc; i++)
             target_argv[i - target_cmdline_start] = argv[i];
         target_argv[argc - target_cmdline_start] = NULL;
-        {
-            int i = 0;
-            while (target_argv[i] != NULL) {
-                printf("target_argv[%d]=%s\n", i, target_argv[i]);
-                i++;
-            }
+
+        if (execvp(target_argv[0], &target_argv[0]) == -1) {
+            free(target_argv);
+            error_at_line(EXIT_FAILURE, errno, __FILE__, __LINE__, "exec(%s) failed", argv[target_cmdline_start]);
         }
-        if (execvp(target_argv[0], &target_argv[0]) == -1)
-            error_at_line(EXIT_FAILURE, errno, __FILE__, __LINE__, "exec(%s) failed", target_argv[0]);
-        free(target_argv);
     }
+
     return 0;
 }
