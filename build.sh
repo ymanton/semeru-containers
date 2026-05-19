f186e332953b275d79c415c266234dca61614e39
Younes Manton <ymanton@ca.ibm.com>
2026-05-19 12:36:03 -0400

    Add support for building with buildah in build.sh
    If the Dockerfile in the user-specified location ends with a .sh suffix
    we assume it's for building with buildah and we execute the script by
    wrapping it in 'buildah unshare' because we need to be able to mount
    container filesystems on the host and to do that as non-root requires
    that we enter a user namespace, which 'buildah unshare' accomplishes.
    
    Signed-off-by: Younes Manton <ymanton@ca.ibm.com>

--- build.sh
+++ build.sh
@@ -19,9 +19,10 @@
 #  Script to build a docker image                                                   #
 #                                                                                   #
 #                                                                                   #
-#  Usage : build.sh <Image name> <Dockerfile location>                              #
+#  Usage : build.sh <Image name> <Dockerfile location> <criu secrets file>          #
 #                                                                                   #
 #####################################################################################
+set -o pipefail
 
 image=$1
 dloc=$2
@@ -52,14 +53,30 @@ then
 fi
 
 echo "******************************************************************************"
-echo "           Starting docker build for $image                                   "
+echo "           Starting build for $image                                          "
 echo "******************************************************************************"
 
 if [[ $VAR == *"17-ea"* ]]; then
   containerEngine=podman
 fi
 
-$containerEngine build --no-cache --pull -t $image -f $dfile $dloc --secret id=criu_secrets,src=$criu_secrets 2>&1 | tee build_$tag.log
+# Check if the Dockerfile is actually a shell script (for buildah-based builds)
+if [ -f "$dfile.sh" ]; then
+    dfile="$dfile.sh"
+    echo "Detected buildah script: $dfile"
+    echo "Executing buildah build..."
+    
+    # Export variables needed by the buildah script
+    export FINAL_IMAGE_NAME="$image"
+    export CRIU_SECRETS_FILE="$criu_secrets"
+    
+    # Execute the buildah script in a user namespace (via `buildah unshare`) so that we can mount
+    # the container filesystem to the host without needing to be root.
+    buildah unshare bash "$dfile" 2>&1 | tee build_$tag.log
+else
+    echo "Using container engine: $containerEngine"
+    $containerEngine build --no-cache --pull -t $image -f $dfile $dloc --secret id=criu_secrets,src=$criu_secrets 2>&1 | tee build_$tag.log
+fi
 
 if [ $? = 0 ]
 then
